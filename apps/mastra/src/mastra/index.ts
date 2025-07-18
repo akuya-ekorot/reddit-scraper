ca6e2b38c68d1b15a8fde2a8e19f77948f010c9a
Akuya Ekorot <akuyaekorot@gmail.com>
2025-07-18 11:46:07 +0300

    feat(mastra): create mastra project

---
+++ apps/mastra/src/mastra/index.ts
@@ -0,0 +1,15 @@
+import { Mastra } from "@mastra/core/mastra";
+import { PinoLogger } from "@mastra/loggers";
+import { LibSQLStore } from "@mastra/libsql";
+
+export const mastra = new Mastra({
+  workflows: {},
+  agents: {},
+  storage: new LibSQLStore({
+    url: ":memory:",
+  }),
+  logger: new PinoLogger({
+    name: "Mastra",
+    level: "info",
+  }),
+});
